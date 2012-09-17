5bc2097e46135dee07782a9011e51f683fb0e99e
Duncan Doyle <Duncan.Doyle@gmail.com>
2012-09-18 01:04:23 +0200

    Initial import.

---
+++ Simple_jBPM_ProcessWeb/src/main/java/org/jbosss/ddoyle/howtojboss/jbpm5/workitemhandlers/HelloServiceJaxWsProxyWorkItemHandler.java
@@ -0,0 +1,21 @@
+package org.jbosss.ddoyle.howtojboss.jbpm5.workitemhandlers;
+
+import org.drools.process.instance.WorkItemHandler;
+import org.drools.runtime.process.WorkItem;
+import org.drools.runtime.process.WorkItemManager;
+
+public class HelloServiceJaxWsProxyWorkItemHandler implements WorkItemHandler {
+
+	@Override
+	public void abortWorkItem(WorkItem arg0, WorkItemManager arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void executeWorkItem(WorkItem arg0, WorkItemManager arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
