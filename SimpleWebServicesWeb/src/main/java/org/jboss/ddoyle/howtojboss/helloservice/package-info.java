32a537e0592c11e6c568db3ca4f4cc23637ccbe7
Duncan Doyle <Duncan.Doyle@gmail.com>
2012-09-28 10:08:33 +0200

    Replaced HelloService and WorldService with PaymentService and OrderService.

--- SimpleWebServicesWeb/src/main/java/org/jboss/ddoyle/howtojboss/helloservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jboss.org/ddoyle/howtojboss/HelloService/")
-package org.jboss.ddoyle.howtojboss.helloservice;
--- SimpleWebServicesWeb/src/main/java/org/jboss/ddoyle/howtojboss/worldservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jboss.org/ddoyle/howtojboss/WorldService/")
-package org.jboss.ddoyle.howtojboss.worldservice;
